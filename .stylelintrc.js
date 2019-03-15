0350953e5a593675181b03941fdd118c319220f8
João Victor Manke <test@idontknow.com>
2019-03-15 04:45:36 -0300

    Setup Stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: 'stylelint-config-standard',
+  rules: {
+    'property-no-unknown': [
+      true,
+      {
+        ignoreProperties: ['composes'],
+      },
+    ],
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global'],
+      },
+    ],
+    'number-leading-zero': 'never',
+    'at-rule-no-vendor-prefix': true,
+    'media-feature-name-no-vendor-prefix': true,
+    'property-no-vendor-prefix': true,
+    'selector-no-vendor-prefix': true,
+    'value-no-vendor-prefix': true,
+  },
+}
